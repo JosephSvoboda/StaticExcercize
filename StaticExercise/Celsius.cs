a93c7072844020c9cd2e6ebc2a538b53e5ea7e89
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-15 15:37:30 -0400

    final commit

---
+++ StaticExercise/Celsius.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise;
+
+public static class TempConverter
+{
+    public static double FahrenheitToCelsius(double fahrenheit)
+    {
+        double a = (fahrenheit - 32) * 5 / 9;
+        return a;
+    }
+    public static double CelsiusToFahrenheit(double celsius)
+    {
+        double answer = (celsius * 9) / 5 + 32;
+        return answer;
+    }
+}
---
+++ StaticExercise/Program.cs
@@ -0,0 +1,9 @@
+﻿using StaticExercise;
+
+
+
+var celsius = TempConverter.FahrenheitToCelsius(113);
+var fahrenheit = TempConverter.CelsiusToFahrenheit(45);
+
+Console.WriteLine($"{celsius}");
+Console.WriteLine($"{fahrenheit}");
